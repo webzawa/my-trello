9e1f0c3a15d58642ae412f72f5d73de21afc5730
webzawa <35700309+webzawa@users.noreply.github.com>
2022-08-11 21:16:41 +0900

    ListAdd コンポーネント 雛形作成

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,10 +3,16 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list-add />
     </main>
   </div>
 </template>
 
 <script>
-export default {};
+import ListAdd from "./ListAdd.vue";
+export default {
+  components: {
+    ListAdd,
+  },
+};
 </script>
