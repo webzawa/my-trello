f6d9b7514d706005bf62b8a34078dff159984931
webzawa <35700309+webzawa@users.noreply.github.com>
2022-08-13 04:28:54 +0900

    ListAdd Formフォーカス時に活性化

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,10 +1,12 @@
 <template>
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
     <button type="submit" class="add-button">Add</button>
   </form>
@@ -15,12 +17,28 @@ export default {
   data: function () {
     return {
       title: "",
+      isEditing: false,
     };
+  },
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      return classList;
+    },
   },
   methods: {
     addList: function () {
       this.$store.dispatch("addlist", { title: this.title });
       this.title = "";
+    },
+    startEditing: function () {
+      this.isEditing = true;
+    },
+    finishEditing: function () {
+      this.isEditing = false;
     },
   },
 };
