634b5d0b40d522535f681a0fff9226c3c553e4ef
webzawa <35700309+webzawa@users.noreply.github.com>
2022-08-13 23:28:16 +0900

    Cardコンポーネント

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,20 +4,33 @@
       <p class="list-title">{{ title }}</p>
       <div class="deletelist" @click="removeList">X</div>
     </div>
+    <card
+      v-for="(item, index) in cards"
+      :body="item.body"
+      :key="item.id"
+      :cardIndex="index"
+      :listIndex="listIndex"
+    />
     <card-add :listIndex="listIndex" />
   </div>
 </template>
 
 <script>
 import CardAdd from "./CardAdd.vue";
+import Card from "./Card.vue";
 
 export default {
   components: {
     CardAdd,
+    Card,
   },
   props: {
     title: {
       type: String,
+      required: true,
+    },
+    cards: {
+      type: Array,
       required: true,
     },
     listIndex: {
