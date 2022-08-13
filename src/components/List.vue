ce2a067bd4751beb4acce0f66dd5b6d98e2b0a58
webzawa <35700309+webzawa@users.noreply.github.com>
2022-08-13 23:57:50 +0900

    List カードをリスト間でもドラッグ&ドロップで移動できる機能を実装

--- src/components/List.vue
+++ src/components/List.vue
@@ -5,7 +5,7 @@
       <p class="list-counter">total: {{ totalCardInList }}</p>
       <div class="deletelist" @click="removeList">X</div>
     </div>
-    <draggable>
+    <draggable group="cards">
       <card
         v-for="(item, index) in cards"
         :body="item.body"
